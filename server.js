e846ce9f3a7c81146596142132d5344db5b18f5b
rmclaughlin <rmclaughlin@appbroker.biz>
2018-02-06 09:04:51 +0000

    Update to ES6 Signed-off-by: rmclaughlin <rmclaughlin@appbroker.biz>

--- server.js
+++ server.js
@@ -23,43 +23,43 @@ var port     = process.env.PORT || 8080; // set our port
 var router = express.Router();
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
+router.use((req, res, next) => {
 	// do logging
 	console.log('Logging activated.');
 	next();
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
 
 //Method to return jacobsthal
-function returnJacobsthal(n){
+const returnJacobsthal(n) => {
 	return Math.pow(2, n) + Math.pow(-1, n);
 }
 
-function getSum(total, num) {
+const getSum(total, num) => {
     return total + num;
 }
 // returns value based on /jacobsthal at nth
 //https://en.wikipedia.org/wiki/Jacobsthal_number
 // ----------------------------------------------------
 router.route('/jacobsthal/:nth')
-	.get(function(req, res) {
+	.get((req, res) => {
 		res.json({description:'The jacobsthal value at nth position', value: returnJacobsthal(req.params.nth) });
 	});
 
 // Calculates the value of the series of jacobsthal numbers up to an index but omits values divisible by 5 from the calculation
 // ----------------------------------------------------
 router.route('/jacobsthal/:nth/:calculateSeries')
-	.get(function(req, res) {
+	.get((req, res) => {
 		var sum = Array.from(Array(parseInt(req.params.calculateSeries)).keys())
-		.map(function(item, index){
+		.map((item, index) => {
 			return returnJacobsthal(index);
 		})
-		.filter(function(item){
+		.filter((item) => {
 			return !(item % 5 === 0) ;
 		})
 		.reduce(getSum);
